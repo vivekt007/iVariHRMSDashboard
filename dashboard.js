38cb50805a3dc46731d586ab79b76c0f972cfd68
vivekt007 <vivektigadi59@gmail.com>
2024-07-17 12:14:46 +0530

    dashboard creation start

---
+++ dashboard.js
@@ -0,0 +1,14 @@
+ var dropdown = document.getElementsByClassName("dropdown-btn");
+        var i;
+
+        for (i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === "flex") {
+                    dropdownContent.style.display = "none";
+                } else {
+                    dropdownContent.style.display = "flex";
+                }
+            });
+        }
